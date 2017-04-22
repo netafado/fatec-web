c224637d403fda87eda899c559ed46f1efc07fd5
Isaias F. dos Santos <isaias.fran@gmail.com>
2017-04-21 22:43:25 -0300

    organização

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,21 @@
 var gulp = require('gulp'),
-    stylus = require('gulp-stylus');
-
-
-gulp.task( 'stylus', function (){
+    stylus = require('gulp-stylus'),
+    autoprefixer = require( 'autoprefixer-stylus' );
+    
+gulp.task( 'stylus', ['print'], function (){
     gulp.src('./lib/stylus/*.styl')
-    .pipe( stylus() )
+
+    .pipe( stylus( {
+        use: autoprefixer(),
+        compress: true
+    }     
+    ) )
     .pipe(gulp.dest('./css/'));
 } );
+
+gulp.task('print', function(){
+    console.log('fez');
+});
+gulp.task('w', function(){
+    gulp.watch(['./lib/stylus/*.styl'], ['stylus']);
+});
