5a3ac9a09ddb25a876011f4215bef2569b144902
Isaias F. dos Santos <isaias.fran@gmail.com>
2017-05-04 21:02:31 -0300

    add

--- app.js
+++ app.js
@@ -27,6 +27,16 @@ var nav = [
 	{
 		link:'/sobre',
 		texto: 'sobre nós'
+	},
+
+	{
+		link:'/asteroids',
+		texto: 'Asteroids'
+	},
+
+	{
+		link:'/pong/sala',
+		texto: 'Pong'
 	}
 
 ];
@@ -40,46 +50,145 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 // pasta para os arquivos staticos
-app.use( express.static('public') );
-app.use( express.static('pong') );
+app.use( express.static( __dirname + '/public') );
+//app.use( express.static( __dirname + '/pong') );
 
 // dinamicas
-app.use( '/pong', pongRouter );
 app.use( '/', indexRouter );
+app.use( '/pong', pongRouter );
 
 
 http.listen(3000);
+
+
 
 var window = {
 	w: 800,
 	h: 500
 }
-var ball = {
-	x: window.w / 2,
-	y: window.h / 2,
-	size: 25,
-	vx: 3,
-	vy: 1
+function Ball (){
+	this.x =  window.w / 2,
+	this.y =  window.h / 2,
+	this.size =  25,
+	this.vx= 3,
+	this.vy= 1
 }
-var players = [];
-var id = 0;
-class Raquete{
-	constructor( x, y, tamanho, id)
-	{
-		this.size = {w: 20, h: tamanho};
 
-		this.id = id;
+
+Ball.prototype = {
+	update: function(){
+		this.x += this.vx;
+		this.y += this.vy;
+
+		if( this.x < 0 )
+			this.vx *= -1;
+
+		if( this.x > window.w)
+			this.vx *= -1;
+
+		if( this.y  < 0)
+			this.vy *= -1;
+
+		if( this.y > window.h)
+			this.vy *= -1;
+	}
+}
+
+
+function Game(id_sala){
+	this.players = [];
+	this.ball = new Ball();
+	this.id = id_sala;
+	this.time;
+
+}
+
+Game.prototype = {
+	update: function(){
+		this.ball.update();
+	}, 
+	addPlayer: function(player){
+		this.players.push(player);
+		
+	},
+	init: function(){
+		var self = this;
+		
+		this.time = setInterval(function(){
+  			self.update();
+
+			  self.players[0].io.emit('update', {
+  					bola: self.ball,
+  					w: window,
+					id: self.id,
+					players: self.players
+  			 	});
+			
+			  for( var i = 0; i < self.players[i].length; i++)
+			  {
+				  console.log('teste');
+				  self.players.io.emit('update', {
+  					bola: self.ball,
+  					w: window,
+					id: self.id,
+					players: self.players
+  			 	});
+			  };
+
+
+  		}, 1000 / 10);		  
+	},
+	parar: function(){
+		clearInterval(this.time.bind(this));
+	}
+}
+
+var sala_id = 0;
+var player_id = 1;
+var salas = [];
+var players = [];
+
+// sala
+function Sala(game, id){
+	this.game = game;
+	this.id = id;
+}
+
+
+
+
+
+
+
+var id = 0;
+class Player{
+	constructor( x, y, tamanho, id, socket_id)
+	{
+		if(id === 1){
+			this.x = 0;
+			this.y = 50;
+		}
+
+		if(id === 2){
+			this.x = 500;
+			this.y = 50;
+		}
+
+		this.w = 30;
+		this.h = 120;
+		this.size = {w: 20, h: tamanho};
+		this.id = id = id + 1;
 		this.color = 'white';
+		this.io = socket_id;
+		console.log(this.io.emit);
 	}
 
-	update()
+	update(posicao)
 	{
 		var velX = 0;
 		var velY = 0;
 
-		var vel = new Vetor2d(velX, velY);
-
-		this.pos.add(vel);
+		
 
 	}
 
@@ -110,8 +219,23 @@ function update(){
 // bola se movimenta 
 //var bola = new Bola(20, 20, 20, 20);
 // no lado do cliente emitira uma mensagem toda vez que a raquete se movimentar
+var ids = 0;
+var currentIO;
 io.on('connection', function(socket){
+currentIO = socket;
+socket.id = ids++;
 
+// ao criar a sala
+socket.on('criarSala', function(data){
+	
+	sala_id +=1;
+	
+	salas[sala_id] = new Sala(new Game(sala_id), sala_id);
+	console.log(salas[sala_id]);
+	socket.emit('salaCriada', { sala: sala_id });
+
+
+});
 
 
   socket.emit('initGame', {
@@ -119,15 +243,35 @@ io.on('connection', function(socket){
   	h: window.h
   });
 
-  setInterval(function(){
 
-  	update();
+  
+// ao criar a sala
+socket.on('add_player', function(data){
 
+	salas[data.id_sala].game.addPlayer(new Player(20, 200, 100, player_id, currentIO));
 
+	if(salas[data.id_sala].game.players.length > 1)
+	{
+		
+		salas[data.id_sala].game.init(socket);
 
-  	socket.emit('update', {
-  		bola: ball,
-  		w: window
-  	 });
-  }, 1000 / 30);
+	}
+	player_id ++;
+
+	if(player_id >= 3)
+		player_id = 1;
+		
+	
+});
+  function comecar(){
+	  setInterval(function(){
+
+		io.emit('update', {
+			bola: ball,
+			w: window
+		});
+		}, 1000 / 30);
+	}
+
+	
 });
--- src/routes/pongRoute.js
+++ src/routes/pongRoute.js
@@ -3,10 +3,22 @@ var express =  require("express");
 var routePong =  express.Router();
 
 function router(nav){
+
 	routePong.route('/')
 	.get( function(req, res){
 		res.render('pong', {nav});
 	});
+
+	routePong.route('/sala')
+	.get(function(req, res){
+		res.render('pongSala', {nav});
+	});
+
+	routePong.route('/sala/:id?')
+	.get(function(req, res){
+		res.render('pong', {nav, sala_id : req.params.id});
+	});
+	
 	return routePong;
 }
 
